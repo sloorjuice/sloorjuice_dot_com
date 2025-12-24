5695339e17489b0fbb8e35fac336dd0f8bd4be77
sloorjuice <antant8085@gmail.com>
2025-12-23 20:52:59 -0500

    css: cleaning up general look

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,21 @@ export default function Home() {
       
       <main className="flex flex-col min-h-screen items-center p-5 gap-5">
         <div className="text-center">
-          <h1 className="text-3xl font-bold underline">Hello, Internet!</h1>
-          <p className="mt-2">Welcome to my website. My name&apos;s Ant but here I go by sloorjuice..</p>
+          <h1 className="text-4xl font-bold">Hello, Internet!</h1>
+          <hr className="w-48 mx-auto mt-2 border-t-2 border-black" /> {/*separator */}
+          <p className="text-lg mt-2">Welcome to my website. My name&apos;s Ant but here I go by sloorjuice..</p>
+          <p className="text-lg mx-120">I&apos;m super passionate about everything computers, but I love game & software development the most.</p>
+        </div>
+
+        <div className="text-center p-5">
+          <h1 className="text-3xl font-bold"> Recent Blogs</h1>
+          <hr className="w-16 mx-auto mt-2 border-t-2 border-black" /> {/*separator */}
         </div>
 
         <div className="text-center border-2 border-black p-5">
           <p> WEBSITE WORK-IN-PROGRESS </p>
         </div>
+
 
       </main>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="flex flex-col items-center justify-center border-b-2 border-black bg-[#a890b6] dark:bg-[#665170]">
-      <h1 className="text-5xl font-medium m-2 mb-0">sloor.dev</h1>
-      <h2 className="text-md font-light m-1 underline">Everything Sloorjuice.</h2>
+    <div className="flex flex-col items-center justify-center border-b-2 border-black bg-[#9884af] dark:bg-[#4e2d6c]">
+      <h1 className="text-6xl font-bold m-2 mb-0">sloor.dev</h1>
+      <h2 className="text-md underline">Everything Sloorjuice.</h2>
       <nav className="mt-2 flex gap-4">
-        <Link href="/" className="hover:underline">Home</Link>
-        <Link href="/portfolio" className="hover:underline">Portfolio</Link>
-        <Link href="/about" className="hover:underline">About</Link>
-        <Link href="/blog" className="hover:underline">Blog</Link>
+        <Link href="/" className="hover:underline text-lg">Home</Link>
+        <Link href="/portfolio" className="hover:underline text-lg">Portfolio</Link>
+        <Link href="/about" className="hover:underline text-lg">About</Link>
+        <Link href="/blog" className="hover:underline text-lg">Blog</Link>
         {/* <Link href="/projects" className="hover:underline">Projects</Link> */}
         {/* <Link href="/jukebox" className="hover:underline">Jukebox</Link> */}
       </nav>
